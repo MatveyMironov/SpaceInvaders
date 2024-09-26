4576b351325e24d90c9e94e203c3d40b2ef998c8
ITHUB\Student <>
2024-09-26 15:32:57 +0300

    Better imlementation

--- SpaceInvadersGame/Assets/_Presentation/Movement Field/Movement Field.cs
+++ SpaceInvadersGame/Assets/_Presentation/Movement Field/Movement Field.cs
@@ -5,17 +5,30 @@ public class MovementField : MonoBehaviour
     [SerializeField] private Transform upperLeftCorner;
     [SerializeField] private Transform lowerRightCorner;
 
-    public bool CheckIfPositionIsOnField(Vector3 position)
+    public Vector3 ClampPositionToField(Vector3 position)
     {
-        if (position.x > upperLeftCorner.position.x
-            && position.x < lowerRightCorner.position.x
-            && position.y < upperLeftCorner.position.y
-            && position.y > lowerRightCorner.position.y
-            )
+        Vector3 clampedPosition = position;
+
+        if (position.x < upperLeftCorner.position.x)
         {
-            return true;
+            clampedPosition.x = upperLeftCorner.position.x;
         }
 
-        return false;
+        if (position.x > lowerRightCorner.position.x)
+        {
+            clampedPosition.x = lowerRightCorner.position.x;
+        }
+
+        if (position.y > upperLeftCorner.position.y)
+        {
+            clampedPosition.y = upperLeftCorner.position.y;
+        }
+
+        if (position.y < lowerRightCorner.position.y)
+        {
+            clampedPosition.y= lowerRightCorner.position.y;
+        }
+
+        return clampedPosition;
     }
 }
--- SpaceInvadersGame/Assets/_Presentation/Player/PlayerMovement.cs
+++ SpaceInvadersGame/Assets/_Presentation/Player/PlayerMovement.cs
@@ -5,9 +5,6 @@ public class PlayerMovement
     public void MovePlayer(Transform player, int horizontalDirection, float speed, MovementField movementField)
     {
         Vector3 newPosition = player.position + new Vector3(horizontalDirection * speed * Time.deltaTime, 0f, 0f);
-        if (movementField.CheckIfPositionIsOnField(newPosition))
-        {
-            player.position = newPosition;
-        }
+        player.position = movementField.ClampPositionToField(newPosition);
     }
 }
